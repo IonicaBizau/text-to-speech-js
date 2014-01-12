6990cebda0bbf3a8da8824be253398f48e63383d
Ionică Bizău <bizauionica@yahoo.com>
2014-01-12 13:16:48 +0200

    Include main.js file

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+// window on load
+window.onload = function () {
+
+    // get run code button
+    document.getElementById("run-code").addEventListener("click", function () {
+        // evaluate the code to run
+        eval(document.getElementById("js-code-to-run").innerText);
+    });
+};
