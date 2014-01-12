f64a25302b638ce21ce9ef661b8c1d561ebaa886
Ionică Bizău <bizauionica@yahoo.com>
2014-01-12 13:18:35 +0200

    Use innerHTML to be firefox compatible

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,6 @@ window.onload = function () {
     // get run code button
     document.getElementById("run-code").addEventListener("click", function () {
         // evaluate the code to run
-        eval(document.getElementById("js-code-to-run").innerText);
+        eval(document.getElementById("js-code-to-run").innerHTML);
     });
 };
